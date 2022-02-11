72ab07f6f34295bde8b5791b2c388d2c442df457
swedishsailor <kzygowski7@gmail.com>
2022-02-11 03:01:46 +0100

    webpack init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Test import of a JavaScript module
+import { example } from '@/js/example'
+
+// Test import of an asset
+import webpackLogo from '@/images/webpack-logo.svg'
+
+// Test import of styles
+import '@/styles/index.scss'
+
+
