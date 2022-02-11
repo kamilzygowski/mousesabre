2bddb4cb5879ef1f11fc0a2cd3d5f1d0905806c4
swedishsailor <kzygowski7@gmail.com>
2022-02-11 04:53:26 +0100

    player and enemies interfaces

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+var interval = 0;
 var mouseImg = new Image(64, 64);
 mouseImg.src = 'https://i.postimg.cc/sDSjbD7K/mouse.png';
 ctx.fillStyle = '#c7ecee';
@@ -15,9 +16,22 @@ var nextFrameBegin = function () {
  * Canvas Rendering
  */
 setInterval(function () {
+    nextFrameBegin();
+    var newEnemy = {
+        img: new Image(),
+        width: 40,
+        height: 40,
+        x: Math.floor(Math.random() * window.innerWidth),
+        y: 0
+    };
+    interval++;
+    if (interval % 1356) {
+        console.log(newEnemy);
+        newEnemy.img.src = 'https://i.postimg.cc/MZ05K17Q/enemy.png';
+    }
+    ctx.drawImage(newEnemy.img, newEnemy.x, newEnemy.y, newEnemy.width, newEnemy.height);
+    newEnemy.y++;
     onmousemove = function (e) {
-        nextFrameBegin();
-        console.log(e.clientX, e.clientY);
         ctx.drawImage(mouseImg, e.clientX - mouseImg.width / 2, e.clientY - mouseImg.height / 2);
     };
 }, 1000 / 60);
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,12 +1,31 @@
+interface Cursor{
+    img:HTMLImageElement;
+    readonly width:number;
+    readonly height:number;
+    x?:number;
+    y?:number;
+}
+
+interface Enemy{
+    img:HTMLImageElement;
+    width:number;
+    height:number;
+    x:number;
+    y:number;
+}
+
 const canvas: HTMLElement = document.getElementById('canvas');
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let interval:number = 0;
+
 const mouseImg: HTMLImageElement = new Image(64, 64);
 mouseImg.src = 'https://i.postimg.cc/sDSjbD7K/mouse.png';
 ctx.fillStyle = '#c7ecee';
+
 
 /**
  * Functions
@@ -20,9 +39,23 @@ const nextFrameBegin = (): void => {
  */
 
 setInterval((): void => {
+    nextFrameBegin();
+    
+    const newEnemy:Enemy = {
+        img : new Image(),
+        width:40,
+        height:40,
+        x:Math.floor(Math.random() * window.innerWidth),
+        y:0
+    }
+    interval++;
+    if(interval%1356){
+        console.log(newEnemy)
+        newEnemy.img.src = 'https://i.postimg.cc/MZ05K17Q/enemy.png';
+    }
+    ctx.drawImage(newEnemy.img, newEnemy.x, newEnemy.y, newEnemy.width, newEnemy.height);
+    newEnemy.y++;
     onmousemove = (e: MouseEvent): void => {
-        nextFrameBegin();
-        console.log(e.clientX, e.clientY);
         ctx.drawImage(mouseImg, e.clientX - mouseImg.width / 2, e.clientY - mouseImg.height / 2);
     }
 }, 1000 / 60);
